169e21c100c86b80b7169866cea02734aa8f6e53
Piyushshind <piyushshinde.199@gmail.com>
2023-06-06 10:43:54 +0530

    prac

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  
+  const [ka, ser] = useState(1)
+  function handle (){
+    ser(a=>a+(a/2))
+    console.log("handle run");
+  }
+  console.log(ka , 'rerender');
   return (
     <>
-    
+       <button
+       onClick={handle}
+       >po</button>
+       {ka}
     </>
   );
 }
