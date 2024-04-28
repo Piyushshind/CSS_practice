97cf8191eabd5d74d93c0e3a80beec7e83d405e4
Piyushshind <piyushshinde.199@gmail.com>
2024-04-28 19:26:01 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  
+  const [first, setfirst] = useState(true)
+  function handle(){
+    setfirst(!first)
+  }
   return (
     <>
-        hello
+        <button onClick={handle}>cjk</button>
+        {first ? <div>rdd  </div>: <div>pink</div>}
     </>
   );
 }
