a77abc503255cadb1eed1d00b6cf6217324944f4
Piyushshind <piyushshinde.199@gmail.com>
2025-08-31 11:18:09 +0530

    flex

--- src/tailwind/PracticeTailwind.jsx
+++ src/tailwind/PracticeTailwind.jsx
@@ -8,13 +8,13 @@ const PracticeTailwind = () => {
     }
     return (
         <>
-            <div className='flex flex-col m-4 p-8'>
-                <div className='text-blue-500 text-4xl '>
+            <div className='flex flex-col m-4 p-8 '>
+                <div className='flex text-blue-500 text-4xl justify-center'>
                     <button className='bg-blue-50 rounded-lg font-bold' onClick={handle}>Click me</button>
                 </div>
-                <div className='border-blue-700'>
+                <div className=' flex border-blue-700 justify-center '>
                     {first ? <span className='bg-red-500 rounded-lg m-2 p-1'>red</span> : 
-                    <span className='bg-green-500 rounded-lg'>green</span>}
+                    <span className='bg-green-500 rounded-lg m-5 p-1'>green</span>}
                 </div>
             </div>
         </>
