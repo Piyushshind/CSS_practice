c483188d092b1fbcbb3027719ae923197b21fb2f
Piyushshind <piyushshinde.199@gmail.com>
2025-09-01 15:32:08 +0530

    border

--- src/tailwind/PracticeTailwind.jsx
+++ src/tailwind/PracticeTailwind.jsx
@@ -8,17 +8,17 @@ const PracticeTailwind = () => {
     }
     return (
         <>
-            <div className='flex flex-col m-4 p-8 '>
+            <div className='flex flex-col m-4 p-8 bg-sky-100  border-5 border-violet-600'>
                 <div className='flex text-blue-500 text-4xl justify-center'>
-                    <button className='bg-blue-50 rounded-lg font-bold' onClick={handle}>Click me</button>
+                    <button className='bg-sky-50 rounded-lg font-bold' onClick={handle}>Click me</button>
                 </div>
-                <div className=' flex border-blue-700 justify-center '>
-                    {first ? <span className='bg-red-500 rounded-lg m-2 p-1'>red</span> : 
-                    <span className='bg-green-500 rounded-lg m-5 p-1'>green</span>}
+                <div className=' flex border-blue-700 justify-center mt-5 '>
+                    {first ? <span className='bg-red-500 rounded-lg p-2 '>red</span> : 
+                    <span className='bg-green-500 rounded-lg p-2'>green</span>}
                 </div>
             </div>
         </>
-    );
+    ); 
 }
 
 export default PracticeTailwind
