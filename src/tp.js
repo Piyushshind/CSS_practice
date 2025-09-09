997e91c8279b56adc713f9103baefdbe7002063c
Piyushshind <piyushshinde.199@gmail.com>
2025-09-09 09:36:40 +0530

    delete keyword

--- src/tp.js
+++ src/tp.js
@@ -1,14 +1,14 @@
 const obj = {
   name: "Alice",
-  9:"pa",
+  x: 5,
   age: 25,
   greet() { return `Hi, I'm ${this.name}`; }
 };
 
-// console.log(Object.getOwnPropertyDescriptor(obj,'name'));
 
-// console.log(Object.defineProperty(obj, 'x', { value: 10, writable: false, enumerable: false, configurable: false }));
-// console.log(obj.greet);
+console.log(Object.defineProperty(obj, 'name', { value: 10, writable: false, enumerable: false, configurable: false }));
+console.log(Object.getOwnPropertyDescriptor(obj, 'x'));
+console.log(obj);
 
 // Object.defineProperties(...)
 const user = {
@@ -16,8 +16,8 @@ const user = {
   get full() { return `${this.first} ${this.last}`; },
   set full(name) { [this.first, this.last] = name.split(' '); }
 };
-console.log(user.full); // "Alice Brown"
-user.full = "Alice Brown";
-console.log(user.full); // "Alice Brown"
+// console.log(user.full); // "Alice Brown"
+// user.full = "Alice Brown";
+// console.log(user.full); // "Alice Brown"
 
 
